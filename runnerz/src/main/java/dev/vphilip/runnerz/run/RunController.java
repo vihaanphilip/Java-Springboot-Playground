ec547b7395f3b168c9b2bb5729c8cdfc7df939a2
Vihaan Philip <vihaanphilip@gmail.com>
2025-09-13 15:23:21 +0800

    Added some validation logic

--- runnerz/src/main/java/dev/vphilip/runnerz/run/RunController.java
+++ runnerz/src/main/java/dev/vphilip/runnerz/run/RunController.java
@@ -1,5 +1,6 @@
 package dev.vphilip.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -38,13 +39,13 @@ public class RunController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run,id);
     }
 
