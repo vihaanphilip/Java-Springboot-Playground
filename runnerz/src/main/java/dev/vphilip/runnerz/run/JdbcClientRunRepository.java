ef355bc257088c23c1f3bc2b6347b9a608b49439
Vihaan Philip <vihaanphilip@gmail.com>
2025-09-15 02:17:53 +0800

    Used Spring Data JDBC functionality for DB access

---
+++ runnerz/src/main/java/dev/vphilip/runnerz/run/JdbcClientRunRepository.java
@@ -0,0 +1,120 @@
+package dev.vphilip.runnerz.run;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JdbcClientRunRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcClientRunRepository.class);
+
+    private final JdbcClient jdbcClient;
+
+    public JdbcClientRunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Run> findAll() {
+        String sql = "SELECT * from run";
+        return jdbcClient.sql(sql)
+                .query(Run.class)
+                .list();
+    }
+
+    public Optional<Run> findById(Integer id) {
+        return jdbcClient.sql("SELECT id,title,started_on,completed_on,miles,location FROM Run WHERE id = :id" )
+                .param("id", id)
+                .query(Run.class)
+                .optional();
+    }
+
+    public void create(Run run) {
+        var updated = jdbcClient.sql("INSERT INTO Run(id,title,started_on,completed_on,miles,location) values(?,?,?,?,?,?)")
+                .params(List.of(run.id(),run.title(),run.startedOn(),run.completedOn(),run.miles(),run.location().toString()))
+                .update();
+
+        Assert.state(updated == 1, "Failed to create run " + run.title());
+    }
+
+    public void update(Run run, Integer id) {
+        var updated = jdbcClient.sql("update run set title = ?, started_on = ?, completed_on = ?, miles = ?, location = ? where id = ?")
+                .params(List.of(run.title(),run.startedOn(),run.completedOn(),run.miles(),run.location().toString(), id))
+                .update();
+
+        Assert.state(updated == 1, "Failed to update run " + run.title());
+    }
+
+    public void delete(Integer id) {
+        var updated = jdbcClient.sql("delete from run where id = :id")
+                .param("id", id)
+                .update();
+
+        Assert.state(updated == 1, "Failed to delete run " + id);
+    }
+
+    public int count() {
+        return jdbcClient.sql("select * from run").query().listOfRows().size();
+    }
+
+    public void saveAll(List<Run> runs) {
+        runs.stream().forEach(this::create);
+    }
+
+    public List<Run> findByLocation(String location) {
+        return jdbcClient.sql("select * from run where location = :location")
+                .param("location", location)
+                .query(Run.class)
+                .list();
+    }
+
+//    private List<Run> runs = new ArrayList<>();
+//
+//    List<Run> findAll(){
+//        return runs;
+//    }
+//
+//    Optional<Run> findById(int id){
+//        return runs.stream()
+//                .filter(run -> run.id() == id)
+//                .findFirst();
+//    }
+//
+//    void create(Run run){
+//        runs.add(run);
+//    };
+//
+//    void update(Run run, Integer id){
+//        Optional<Run> existingRun = findById(id);
+//        existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
+//    };
+//
+//    void delete(Integer id){
+//        runs.removeIf(run -> run.id().equals(id));
+//    };
+//
+//
+//    @PostConstruct
+//    private void init(){
+//        runs.add(new Run(1,
+//                "Monday Morning Run",
+//                LocalDateTime.now(),
+//                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+//                5,
+//                Location.OUTDOOR)
+//        );
+//
+//        runs.add(new Run(2,
+//                "Wednesday Morning Run",
+//                LocalDateTime.now(),
+//                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+//                10,
+//                Location.INDOOR)
+//        );
+//    }
+}
--- runnerz/src/main/java/dev/vphilip/runnerz/run/Run.java
+++ runnerz/src/main/java/dev/vphilip/runnerz/run/Run.java
@@ -2,10 +2,14 @@ package dev.vphilip.runnerz.run;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 
+
 public record Run(
+    @Id
     Integer id,
     @NotEmpty
     String title,
@@ -13,11 +17,13 @@ public record Run(
     LocalDateTime completedOn,
     @Positive
     Integer miles,
-    Location location
+    Location location,
+    @Version
+    Integer version
 ) {
     public Run {
-         if(!completedOn.isAfter(startedOn)) {
-             throw new IllegalArgumentException("Completed On must be after Started On");
-         }
+        if (startedOn != null && completedOn != null && !completedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("Completed On must be after Started On");
+        }
     }
 }
